8556bbae8d9ccdd8fc8dcfad2100a63f8e76074c
Nadya <nb1985@ukr.net>
2024-12-18 09:52:40 +0200

    feat: add client-side pagination to load photos in batches

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,28 @@
-document.addEventListener("DOMContentLoaded", function() {
+
+document.addEventListener("DOMContentLoaded", function () {
     const items = document.querySelectorAll(".w320-images__item");
     const btn = document.querySelector(".w320-images__btn");
 
-    let currentIndex = 1;
+    let currentIndex = 1; 
 
-    function showNextBlock () {
+    function showNextBlock() {
         if (currentIndex < items.length) {
-            items[currentIndex].style.display = "block";
+            const nextItem = items[currentIndex];
+            nextItem.style.display = "block"; 
+            setTimeout(() => nextItem.classList.add("visible"), 10); 
             currentIndex++;
         }
 
         if (currentIndex === items.length) {
-            btn.style.disabled = true;
+            btn.setAttribute("disabled", true); 
         }
     }
 
     items.forEach((item, index) => {
         if (index !== 0) {
-          item.style.display = "none"; 
+            item.style.display = "none"; 
         }
     });
 
-     btn.addEventListener("click", showNextBlock);
+    btn.addEventListener("click", showNextBlock);
 });
